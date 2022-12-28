46b0a5158f22556bdd3a6d71208dfd4726eb0b19
RoshanLiu <xygenio@hotmail.com>
2022-12-28 12:47:27 +1100

    1

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,5 +5,12 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+		string LoginDate = DateTime.Today.ToString("dd/MM/yyyy");
+		if (LoginDate!= Preferences.Default.Get("SAG","Expired"))
+		{
+            Preferences.Default.Set("SAG", "Expired");
+        }
+		
+        
+    }
 }
