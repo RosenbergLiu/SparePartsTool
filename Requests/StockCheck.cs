047b047e24700fcb94abdba6ab9cb02e726df6cc
RoshanLiu <xygenio@hotmail.com>
2022-12-28 15:45:27 +1100

    Create StockCheck.cs

---
+++ Requests/StockCheck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace SparePartsTool.Requests
+{
+    internal class StockCheck
+    {
+        public int CheckPart(string Part, string Cookie) {
+        
+        
+        
+        
+        }
+    }
+}
